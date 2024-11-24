77df2f07262c4d9b9984dac5fe37f64ef60efb32
aberdun <alejandro.berdun@seidor.com>
2024-11-24 14:51:34 +0100

    more changes

--- Metadata Management/Errors/extractFieldError.js
+++ Metadata Management/Errors/extractFieldError.js
@@ -1,17 +1,18 @@
-const { execSync } = require('child_process');
+let { execSync } = require('child_process');
 
 //paso previo, desde la rama correspondiente: sfdx sgd:source:delta --from origin/ramaDestino --output .
-const profilePath = 'C:\\Users\\aberdun\\Downloads\\iberdrola-sfdx\\force-app\\main\\default\\profiles';
+let profilePath = 'C:\\Users\\aberdun\\Downloads\\iberdrola-sfdx\\force-app\\main\\default\\profiles';
+let permissionSetPath = 'C:\\Users\\aberdun\\Downloads\\iberdrola-sfdx\\force-app\\main\\default\\permissionsets';
 //prod: 'sf project deploy start --target-org IBD-prod --manifest C:\\Users\\aberdun\\Downloads\\iberdrola-sfdx\\package\\package.xml --ignore-conflicts --ignore-warnings --dry-run'
 //qa: 'sf project deploy start --target-org QA-IBD --manifest manifest\\package\\package.xml --ignore-conflicts --ignore-warnings -l NoTestRun --dry-run'
-const deployCommand = 'sf project deploy start --target-org IBD-prod --manifest C:\\Users\\aberdun\\Downloads\\iberdrola-sfdx\\package\\package.xml --ignore-conflicts --ignore-warnings --dry-run';
-const deleteScriptTemplate = 'node "C:\\Users\\aberdun\\Downloads\\rm\\Metadata Management\\Errors\\deletePermissionSetProfileFieldpermissionsReferencesByObjectOrField.mjs" "${profilePath}" "${fieldName}"';
-const deleteScriptTemplateRecordType = 'node "C:\\Users\\aberdun\\Downloads\\rm\\Metadata Management\\Errors\\deletePermissionSetProfileRecordTypepermissionsReferences.mjs" "${profilePath}" "${recordTypeName}"';
-const deleteScriptTemplateObject = 'node "C:\\Users\\aberdun\\Downloads\\rm\\Metadata Management\\Errors\\deletePermissionSetProfileObjectpermissionsReferences.mjs" "${profilePath}" "${objectName}"';
+let deployCommand = 'sf project deploy start --target-org IBD-prod --manifest C:\\Users\\aberdun\\Downloads\\iberdrola-sfdx\\package\\package.xml --ignore-conflicts --ignore-warnings --dry-run';
 
+
+let deleteScriptTemplate = 'node "C:\\Users\\aberdun\\Downloads\\rm\\Metadata Management\\Errors\\deletePermissionSetProfileFieldpermissionsReferencesByObjectOrField.mjs" "${profilePath}" "${fieldName}"';
+let deleteScriptTemplateRecordType = 'node "C:\\Users\\aberdun\\Downloads\\rm\\Metadata Management\\Errors\\deletePermissionSetProfileRecordTypepermissionsReferences.mjs" "${profilePath}" "${recordTypeName}"';
+let deleteScriptTemplateObject = 'node "C:\\Users\\aberdun\\Downloads\\rm\\Metadata Management\\Errors\\deletePermissionSetProfileObjectpermissionsReferences.mjs" "${profilePath}" "${objectName}"';
 
 let fieldsFound = true;
-
 try {
     while (fieldsFound) {
         console.log('Starting deployment...');
@@ -23,60 +24,67 @@ try {
             output = error.stdout || '';
             console.error('Deployment failed, processing output...');
         }
-
         console.log('Command output:\n', output);
-
-        // Buscar el texto entre "named" y "found"
-        const fieldRegex = /In field: field - no CustomField named\s+([^\s]+)\s+found/;
-        const fieldMatch = output.match(fieldRegex);
-
-        const recordTypeRegex = /In field: recordType - no RecordType named\s+([^\s]+)\s+found/;
-        const recordTypeMatch = output.match(recordTypeRegex);
-
-        const objectRegex = /In field: field - no CustomObject named\s+([^\s]+)\s+found/;
-        const objectMatch = output.match(objectRegex);
-
+        let fieldRegex = /In field: field - no CustomField named\s+([^\s]+)\s+found/;
+        let fieldMatch = output.match(fieldRegex);
+        let recordTypeRegex = /In field: recordType - no RecordType named\s+([^\s]+)\s+found/;
+        let recordTypeMatch = output.match(recordTypeRegex);
+        let objectRegex = /In field: field - no CustomObject named\s+([^\s]+)\s+found/;
+        let objectMatch = output.match(objectRegex);
         if (fieldMatch && fieldMatch[1]) {
-            const fieldName = fieldMatch[1];
+            let fieldName = fieldMatch[1];
             console.log('Extracted field:', fieldName);
 
-            // Lanzar el script de eliminación con el campo como parámetro
-            const deleteScript = deleteScriptTemplate
+            let deleteScript = deleteScriptTemplate
                 .replace('${profilePath}', profilePath)
                 .replace('${fieldName}', fieldName);
 
             console.log('Running delete script...');
-            const deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
+            let deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
+            console.log('Delete script output:\n', deleteOutput);
+
+            //PS
+             deleteScript = deleteScriptTemplate
+                .replace('${profilePath}', permissionSetPath)
+                .replace('${fieldName}', fieldName);
+
+            console.log('Running delete script...');
+            deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
             console.log('Delete script output:\n', deleteOutput);
         } else if (recordTypeMatch && recordTypeMatch[1]) {
-            const recordTypeName = recordTypeMatch[1];
+            let recordTypeName = recordTypeMatch[1];
             console.log('Extracted recordType:', recordTypeName);
 
-            // Lanzar el script de eliminación con el campo como parámetro
-            const deleteScript = deleteScriptTemplateRecordType
+            let deleteScript = deleteScriptTemplateRecordType
                 .replace('${profilePath}', profilePath)
                 .replace('${recordTypeName}', recordTypeName);
 
             console.log('Running delete script...');
-            const deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
+            let deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
+            console.log('Delete script output:\n', deleteOutput);
+            //PS
+             deleteScript = deleteScriptTemplateRecordType
+                .replace('${profilePath}', permissionSetPath)
+                .replace('${recordTypeName}', recordTypeName);
+
+            console.log('Running delete script...');
+             deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
             console.log('Delete script output:\n', deleteOutput);
         }else if (objectMatch && objectMatch[1]) {
-            const objectName = objectMatch[1];
+            let objectName = objectMatch[1];
             console.log('Extracted objectName:', objectName);
 
-            // Lanzar el script de eliminación con el campo como parámetro
-            const deleteScript = deleteScriptTemplateObject
+            let deleteScript = deleteScriptTemplateObject
                 .replace('${profilePath}', profilePath)
                 .replace('${objectName}', objectName);
 
             console.log('Running delete script...');
-            const deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
+            let deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
             console.log('Delete script output:\n', deleteOutput);
 
-            const fieldName = fieldMatch[1];
+            let fieldName = fieldMatch[1];
             console.log('Extracted field:', fieldName);
 
-            // Lanzar el script de eliminación con el campo como parámetro
             deleteScript = deleteScriptTemplate
                 .replace('${profilePath}', profilePath)
                 .replace('${fieldName}', fieldName);
@@ -84,9 +92,29 @@ try {
             console.log('Running delete script...');
             deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
             console.log('Delete script output:\n', deleteOutput);
+            //PS
+             deleteScript = deleteScriptTemplateObject
+                .replace('${profilePath}', permissionSetPath)
+                .replace('${objectName}', objectName);
+
+            console.log('Running delete script...');
+             deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
+            console.log('Delete script output:\n', deleteOutput);
+
+             fieldName = fieldMatch[1];
+            console.log('Extracted field:', fieldName);
+
+            deleteScript = deleteScriptTemplate
+                .replace('${profilePath}', permissionSetPath)
+                .replace('${fieldName}', fieldName);
+
+            console.log('Running delete script...');
+            deleteOutput = execSync(deleteScript, { encoding: 'utf-8' });
+            console.log('Delete script output:\n', deleteOutput);
+
         }else {
             console.log('No further action required.');
-            fieldsFound = false; // Salir del bucle
+            fieldsFound = false;
         }
     }
 } catch (error) {
